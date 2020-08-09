94b8de9795427faa16afa10412b69356b1f42597
Madison Brown <mbrown3391@gmail.com>
2020-08-09 19:13:40 -0400

    implemented batch image download function

--- server/util.js
+++ server/util.js
@@ -1,10 +1,18 @@
 const fs = require('fs');
+const path = require('path');
 const { promisify } = require('util');
+const download = require('image-downloader');
 const csvStringify = promisify(require('csv-stringify'));
 
 const writeFileAsync = promisify(fs.writeFile);
+const renameAsync = promisify(fs.rename);
 
-const writeCsv = async (filePath, data) => {
+/**
+ * Converts an array of objects ```data``` with the same schema to a CSV format and writes it to a file at ```filePath```. Headers are determined by the keys on the first object.
+ * @param {Array<object>} data
+ * @param {string} filePath
+ */
+const writeCsv = async (data, filePath) => {
   const formatted = [Object.keys(data[0])];
   data.forEach((entry) => formatted.push(Object.values(entry)));
 
@@ -13,4 +21,39 @@ const writeCsv = async (filePath, data) => {
   return writeFileAsync(filePath, csv);
 };
 
-module.exports = { writeCsv };
+/**
+ * Rudimenatary batch downloader (should probably be replaced with something more robust).
+ * @param {Array<object>} sources An array of objects with keys ```url``` and ```name``` - the url of the image to download the name of the file to save it in
+ * @param {string} directory
+ * @param {number} batchSize The number of images to download concurrently.
+ * @returns The number of images successfully downloaded.
+ */
+const downloadImages = async (sources, directory, batchSize) => {
+  let index = 0;
+  let success = 0;
+
+  while (index < sources.length) {
+    // eslint-disable-next-line no-loop-func
+    const batch = sources.slice(index, index + batchSize).map(({ url, name }) => {
+      return download
+        .image({
+          url,
+          dest: directory,
+        })
+        .then(({ filename }) => {
+          const ext = path.extname(filename);
+          renameAsync(filename, `${directory}/${name}${ext}`);
+          console.log(filename);
+          success += 1;
+        });
+    });
+    index += batchSize;
+
+    // eslint-disable-next-line no-await-in-loop
+    await Promise.all(batch);
+  }
+
+  return success;
+};
+
+module.exports = { writeCsv, downloadImages };
